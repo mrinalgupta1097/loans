d2087697da4cb87f86ddbc2e4957a2f626ee5dc7
Mrinal Gupta <mrinalgupta1097@gmail.com>
2023-10-12 01:56:43 +0530

    API documentation - adding description of API

--- src/main/java/com/mrinalgupta/accounts/AccountsApplication.java
+++ src/main/java/com/mrinalgupta/accounts/AccountsApplication.java
@@ -1,14 +1,24 @@
 package com.mrinalgupta.accounts;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-/**
- * The type Accounts application.
- */
+/** The type Accounts application. */
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+    info =
+        @Info(
+            title = "Accounts Microservice REST API Documentation",
+            description = "Bank Accounts microservice REST API Documentaion",
+            version = "v1",
+            contact = @Contact(name = "mrinal gupta", email = "mrinalgupta1097@gmail.com"),
+            license = @License(name = "APACHE 2.0")))
 public class AccountsApplication {
 
   /**
@@ -17,7 +27,6 @@ public class AccountsApplication {
    * @param args the input arguments
    */
   public static void main(String[] args) {
-        SpringApplication.run(AccountsApplication.class, args);
-    }
-
+    SpringApplication.run(AccountsApplication.class, args);
+  }
 }
